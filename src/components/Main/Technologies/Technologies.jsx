b94cd7d20ee1279b9eb71897f73adbc13fb98b1f
isAlvarezDev <alvarezisaac871@gmail.com>
2025-03-07 21:40:41 -0400

    Added ArrowDown svg and added some styles for it

--- src/components/Main/Technologies/Technologies.jsx
+++ src/components/Main/Technologies/Technologies.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 import technologies from "../../../data/technologies.json";
 import "./Technologies.css";
 import { SvgRenderer } from "./SvgRenderer";
+import { ArrowDown } from "../../Svgs";
 
 export const Technologies = ({ translations, currentLang }) => {
   const swapy = useRef(null);
@@ -22,19 +23,7 @@ export const Technologies = ({ translations, currentLang }) => {
       <h2 className="title">{tech}</h2>
       <div className="drag-indication">
         <p>{drag}</p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          fill="#6B7280"
-          className="bi bi-arrow-down-short"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4"
-          />
-        </svg>
+        <ArrowDown />
       </div>
       <div className="skills" ref={container}>
         {technologies.map((technology, index) => {

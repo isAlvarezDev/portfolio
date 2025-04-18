79791a7e3e4781a9397f4a2b022d23b5912f515f
Isaac Alvarez <157077804+isAlvarezDev@users.noreply.github.com>
2025-04-17 22:41:46 -0400

    Added meta description for both languages

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import {
   PageTransition,
   StairTransition,
 } from "./components/";
-import { changeLangAttr } from "./utils";
+import { changeLangAttr, changeMetaDescr } from "./utils";
 
 import translations from './components/i18n/translations.json'
 
 function App() {
   const currentLang = navigator.language.includes('es') ? 'es' : 'en' 
   changeLangAttr(currentLang)
+  changeMetaDescr(currentLang)
   return (
     <>
       <StairTransition></StairTransition>
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
 export const getImageUrl = path => `./assets/${path}`
 
 export const changeLangAttr = (lang) => document.documentElement.setAttribute('lang', lang)
+
+export const changeMetaDescr = (lang) => {
+    const metaDescription = document.head.querySelector('meta[name="description"]')
+
+    if(lang === 'es') {
+        metaDescription.content = 'Desarrollador front-end junior, dedicado a la creación de experiencias digitales de calidad y con gran entusiasmo por aprender y crecer en proyectos desafiantes con equipos dinámicos.'
+    }
+    else {
+        metaDescription.content = 'Junior front-end developer dedicated to creating quality digital experiences, with a strong enthusiasm for learning and growth in challenging projects with dynamic teams.'
+    }
+}

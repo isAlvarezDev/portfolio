5d16e3e30584860df41cb6ab8438b66677da01b2
Isaac Alvarez <157077804+isAlvarezDev@users.noreply.github.com>
2025-10-06 11:51:57 -0400

    Modified several styles for better ui

--- src/components/Svgs/GraduationHat.jsx
+++ src/components/Svgs/GraduationHat.jsx
@@ -1,6 +1,7 @@
 export const GraduationHat = () => {
   return (
-    <svg
+    <a href="https://drive.google.com/file/d/1WqNyw35T5XkqXke20BUWxBjuaCi4-Vh8/view?usp=sharing" target="_blank"  rel="noreferrer">
+      <svg
       xmlns="http://www.w3.org/2000/svg"
       width="32"
       height="32"
@@ -14,5 +15,6 @@ export const GraduationHat = () => {
       <path d="M22 10v6M2 10l10-5 10 5-10 5z"></path>
       <path d="M6 12v5c3 3 9 3 12 0v-5"></path>graduation-hat
     </svg>
+    </a>
   );
 };

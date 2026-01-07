9b226f1c1c281bdac268745ade241f07aa891692
Isaac Alvarez <157077804+isAlvarezDev@users.noreply.github.com>
2026-01-07 09:50:20 -0400

    refactor background styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import { Hero, Technologies, Projects, Formation, About } from "./"
 
 export const Main = ({ translations, currentLang }) => {
-    return <main>
+    return <main style={{backgroundColor:"#04152d"}}>
         <Hero translations={translations} currentLang={currentLang} />
         <Technologies translations={translations} currentLang={currentLang} />
         <Projects translations={translations} currentLang={currentLang} />
